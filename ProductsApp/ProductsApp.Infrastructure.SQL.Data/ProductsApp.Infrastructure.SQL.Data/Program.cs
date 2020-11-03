e447324a07d11c1bd478c3bc9819f9f79c198c7f
abdi2148 <abdiqafar.abas@gmail.com>
2020-11-03 15:45:00 +0100

    SQL folders

---
+++ ProductsApp/ProductsApp.Infrastructure.SQL.Data/ProductsApp.Infrastructure.SQL.Data/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductsApp.Infrastructure.SQL.Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
